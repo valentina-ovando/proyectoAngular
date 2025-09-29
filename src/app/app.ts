321cffe172dfc91f1497bdae3cfb4cd13f6f41fe
valentina-ovando <valentinaovando702@gmail.com>
2025-09-29 17:08:50 -0300

    NUEVOS CAMBIOS ANGULAR

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { About } from "./pages/about/about";
-import { Home } from "./pages/home/home";
-import { Contact } from "./pages/contact/contact"
-import { Products } from "./pages/products/products"
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, About, Home, Contact,Products],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
