45be94f11ea0289a769a94249979694709d1913d
valentina-ovando <valentinaovando702@gmail.com>
2025-09-30 17:03:48 -0300

    cambios nuevos-tarjetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { Contact } from './pages/contact/contact';
 import { Products } from './pages/products/products';
 
 export const routes: Routes = [
+    /*definimos ruta general o raiz en especifico que nos lleve a la ruta home */
+    {path: "**", component: Home },
     {path: `home`, component: Home},
     {path: `about`, component: About},
     {path: `contact`, component: Contact},
