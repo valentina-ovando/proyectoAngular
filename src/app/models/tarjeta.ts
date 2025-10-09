0ed03b32a6fb66f6dfa5185157972d520ca64acd
valentina-ovando <valentinaovando702@gmail.com>
2025-10-09 16:51:48 -0300

    repaso de interfaces y directiva ngFor;aplicamos la unidades de tamaño HTML; aplicamos modal para ver mas informacion de tarjetas

---
+++ src/app/models/tarjeta.ts
@@ -0,0 +1,10 @@
+
+export interface Tarjeta {
+    id: string;
+    img: string;
+    alt:string;
+    titulo:string;
+    descripcion:string;
+    anualPublicacion:number;
+}
+
--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
-
+import { Tarjeta } from '../../models/tarjeta';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
 export class Products {
 
+  public infoTarjeta: Tarjeta[];
+
+  constructor() {
+    this.infoTarjeta = [
+      {
+        id: "1L",
+        img: "../../../assets/pez dragon.jpg",
+        alt: "Es un pez que habita en aguas salobres de America",
+        titulo: "Pez Dragon Gobio",
+        descripcion: "Es un pez que habita en aguas salobres de America",
+        anualPublicacion: 2026,
+      },
+      {
+        id: "2L",
+        img: "../../../assets/pez disco.jpg",
+        alt: "Es un pez de agua dulce originario de las zonas bajas del rio Amazonas.",
+        titulo: "Pez Disco",
+        descripcion: "Es un pez de agua dulce originario de las zonas bajas del rio Amazonas.",
+        anualPublicacion: 2026,
+      },
+      {
+        id: "3L",
+        img: "../../../assets/pez guppy.jpg",
+        alt: "Es un pez de agua dulce.Famoso por su facil cuidado y reproduccion.",
+        titulo: "Pez Guppy",
+        descripcion: "Es un pez de agua dulce. Famoso por su facil cuidado y reproduccion.",
+        anualPublicacion: 2026,
+      },
+    ]
+  }
+
+  /*variable para tomar una tarjeta seleccionada */
+  tarjetaSeleccionada?:any = null;
+
+  /*recibe el elemento seleccionado por parametro y la asigna a "tarjetaSeleccionada" */
+  verMas(tarjeta: any){
+    this.tarjetaSeleccionada = tarjeta;
+  }
 }
